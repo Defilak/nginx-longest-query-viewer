cd2d2cd88b4462711b98e08055e8b74d257ba38e
Maxim <defilak@mail.ru>
2022-09-01 19:54:30 +0300

    добавил счетчик, подправил ридми

--- app.php
+++ app.php
@@ -13,6 +13,8 @@ const REGEX = '/(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(.+?)\s-\s(?:.+\"GET|POS
 
 $count = 0;
 try {
+    $start_time =  hrtime(true);
+
     $result = [];
     while (!feof($fp)) {
         $line = fgets($fp);
@@ -22,18 +24,20 @@ try {
             throw new Exception('Parsing error');
         }
 
-        //При желании можно и по upstream_response_time
         [, $url, $query, $upstream_response_time, $response_time] = $matches;
 
         update_stats($result, $url . $query, $upstream ? $upstream_response_time : $response_time);
         $count++;
     }
 
-    fclose($fp);
+    $delta = (hrtime(true) - $start_time) / 1_000_000_000;
+    print "Время: {$delta}s\n";
     print_result($result);
 } catch (Throwable $ex) {
     print "\e[31m" . $ex->getMessage() . "\n";
     print "Error in line №$count: \n$line\n\e[0m";
+} finally {
+    fclose($fp);
 }
 
 function update_stats(&$result, $url, $time)
@@ -49,7 +53,7 @@ function update_stats(&$result, $url, $time)
 
 function print_result($result)
 {
-    print "Топ 10 тяжелых запросов: \n\n";
+    print "10 самых длительных запросов: \n\n";
     foreach ($result as $url => $time) {
         $out = strlen($url) > 100 ? substr($url, 0, 100) . "..." : $url;
         print "{$time}c. - $out\n";
