0bbfdcf1179406f7d5f0b64128695b7694b8f401
Maxim <defilak@mail.ru>
2022-09-01 20:42:52 +0300

    а, так надо было В СРЕДНЕМ

--- appm.php
+++ appm.php
@@ -1,8 +1,5 @@
 <?php
 
-// Пытался уложится в 100мб, не вышло(
-ini_set('memory_limit', '-1');
-
 $filename = $argv[1] ?? 'obfuscated.log';
 $upstream = $argv[2] ?? false;
 
@@ -14,7 +11,6 @@ if (!$fp) {
 //я устал
 const REGEX = '/(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(.+?)\s-\s(?:.+\"GET|POST|PUT|DELETE)\s(.*?(?=\ HT|HTTP\/\d\.\d|\d")).+\s(\d+(?:\.\d+)?)\s(\d+(?:\.\d+)?)/';
 
-//нужна только для вывода ошибок.
 $count = 0;
 try {
     $start_time =  hrtime(true);
@@ -30,35 +26,34 @@ try {
 
         [, $url, $query, $upstream_response_time, $response_time] = $matches;
 
-        $url_query = $url.$query;
-        if (!isset($result[$url_query])) {
-            $result[$url_query] = [];
-        }
-        $result[$url_query][] = $upstream ? $upstream_response_time : $response_time;
+        update_stats($result, $url . $query, $upstream ? $upstream_response_time : $response_time);
         $count++;
     }
 
-    //Считаю среднее
-    foreach ($result as $key => $entry) {
-        $result[$key] = array_sum($entry) / count($entry);
-    }
-
-    arsort($result);
-    $result = array_slice($result, 0, 10);
-
-    print_result_middle($result, $start_time);
+    $delta = (hrtime(true) - $start_time) / 1_000_000_000;
+    print "Время: {$delta}s\n";
+    print_result($result);
 } catch (Throwable $ex) {
     print "\e[31m" . $ex->getMessage() . "\n";
-    print "Error on line №$count: \n$line\n\e[0m";
+    print "Error in line №$count: \n$line\n\e[0m";
 } finally {
     fclose($fp);
 }
 
-function print_result_middle($result, $start_time)
+function update_stats(&$result, $url, $time)
 {
-    $delta = (hrtime(true) - $start_time) / 1_000_000_000;
-    print "Время: {$delta}s\n";
-    print "10 самых длительных запросов (в среднем): \n\n";
+    if (isset($result[$url]) && $result[$url] > $time)
+        return;
+    $result[$url] = $time;
+    arsort($result);
+    if (count($result) > 10) {
+        array_pop($result);
+    }
+}
+
+function print_result($result)
+{
+    print "10 самых длительных запросов: \n\n";
     foreach ($result as $url => $time) {
         $out = strlen($url) > 100 ? substr($url, 0, 100) . "..." : $url;
         print "{$time}c. - $out\n";
